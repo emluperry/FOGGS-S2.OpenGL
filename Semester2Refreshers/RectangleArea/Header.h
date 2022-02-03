d149c11d3ae48da13f397af50c6d42ebd171965c
Osiristic <emluperry@gmail.com>
2022-02-03 11:50:02 +0000

    T1 Rectangle Area
    Rectangle area task, takes user input and uses this to calculate the area of a rectangle.

---
+++ Semester2Refreshers/RectangleArea/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+void InputRectangle(int& width, int& height);
+void OutputRectangleArea(const int width, const int height);
+void OutputRectanglePerimeter(const int width, const int height);
