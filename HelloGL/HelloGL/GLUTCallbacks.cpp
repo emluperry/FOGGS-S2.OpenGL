7cb06cddaadfd6f0ae4788254934efd4edea992a
Emily Perry <emluperry@gmail.com>
2022-02-08 13:45:15 +0000

    Task 3 - Improving Timer

--- HelloGL/HelloGL/GLUTCallbacks.cpp
+++ HelloGL/HelloGL/GLUTCallbacks.cpp
@@ -24,7 +24,9 @@ namespace GLUTCallbacks
 
 	void Timer(int preferredRefresh)
 	{
+		int updateTime = glutGet(GLUT_ELAPSED_TIME);
 		helloGL->Update();
-		glutTimerFunc(preferredRefresh, GLUTCallbacks::Timer, preferredRefresh);
+		updateTime = glutGet(GLUT_ELAPSED_TIME) - updateTime;
+		glutTimerFunc(preferredRefresh - updateTime, GLUTCallbacks::Timer, preferredRefresh);
 	}
 }
