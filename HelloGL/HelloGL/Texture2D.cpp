2143f2a86cf4cfc6cfd77cfffd315f35086e4185
Osiristic <emluperry@gmail.com>
2022-03-04 00:26:20 +0000

    BMP Loader Attempt 1
    File seems to load, uncertain if it has desired effect.

--- HelloGL/HelloGL/Texture2D.cpp
+++ HelloGL/HelloGL/Texture2D.cpp
@@ -23,6 +23,7 @@ bool Texture2D::Load(char* path, int width, int height)
 	if (!inFile.good())
 	{
 		std::cerr << "Can't open texture file " << path << std::endl;
+		return false;
 	}
 
 	inFile.seekg(0, std::ios::end);
@@ -39,5 +40,63 @@ bool Texture2D::Load(char* path, int width, int height)
 	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, tempTextureData);
 
 	delete[] tempTextureData;
+	return true;
+}
+
+bool Texture2D::LoadBmp(char* path, int width, int height)
+{
+	std::ifstream inFile;
+	_width = width;
+	_height = height;
+	inFile.open(path, std::ios::binary);
+
+	if (!inFile.good())
+	{
+		std::cerr << "Can't open texture file " << path << std::endl;
+	}
+
+	bmpFile_type type;
+	bmpFile_header header;
+	bmp_infoheader infoh;
+	inFile.read((char*)&type, sizeof(bmpFile_type));
+	inFile.read((char*)&header, sizeof(bmpFile_header));
+	inFile.read((char*)&infoh, sizeof(bmp_infoheader));
+	if (infoh.width != infoh.height)
+	{
+		std::cerr << "Bitmap is not square." << std::endl;
+		return false;
+	}
+
+	//create buffer to hold each line as it is read in
+	bmp_color* imageBuffer = new bmp_color;
+
+	std::stringstream stream;
+
+	int index = 0;
+	for (int row = 0; row < infoh.height; row++)
+	{
+		inFile.read((char*)imageBuffer, sizeof(bmp_color));
+		for (int col = 0; col < infoh.width; col++)
+		{
+			stream << imageBuffer->r << imageBuffer->g << imageBuffer->b;
+		}
+	}
+
+	inFile.close();
+	delete imageBuffer;
+
+	stream.seekg(0, std::ios::end);
+	int fileSize = (int)stream.tellg();
+	char* tempTextureData = new char[fileSize];
+	stream.seekg(0, std::ios::beg);
+	stream.read(tempTextureData, fileSize);
+
+	std::cout << path << " loaded." << std::endl;
+
+	glGenTextures(1, &_ID); //get next texture ID
+	glBindTexture(GL_TEXTURE_2D, _ID); //bind texture to ID
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, tempTextureData); //specify details of texture image
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, tempTextureData);
+
 	return true;
 }
--- HelloGL/HelloGL/Texture2D.h
+++ HelloGL/HelloGL/Texture2D.h
@@ -6,6 +6,40 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+
+struct bmpFile_type
+{
+	unsigned char magic[2];
+};
+
+struct bmpFile_header
+{
+	uint32_t filesz;
+	uint16_t creator1;
+	uint16_t creator2;
+	uint32_t bmp_offset;
+};
+
+struct bmp_infoheader
+{
+	uint32_t header_sz;
+	int32_t width;
+	int32_t height;
+	uint16_t nplanes;
+	uint16_t bitspp;
+	uint32_t compress_type;
+	uint32_t bmp_bytesz;
+	int32_t hres;
+	int32_t vres;
+	uint32_t ncolors;
+	uint32_t nimpcolors;
+};
+
+struct bmp_color
+{
+	unsigned char b, g, r;
+};
 
 class Texture2D
 {
@@ -15,7 +49,10 @@ private:
 public:
 	Texture2D();
 	~Texture2D();
+
 	bool Load(char* path, int width, int height);
+	bool LoadBmp(char* path, int width, int height);
+
 	GLuint GetId() const { return _ID; }
 	int GetWidth() const { return _width; }
 	int GetHeight() const { return _height; }
