a28c46debeb97e4707282e17a01303e17f3c66e0
Emily Perry <emluperry@gmail.com>
2022-03-15 13:30:28 +0000

    Learning Linked Lists

---
+++ LinkedLists/LinkedLists.cpp
@@ -0,0 +1,77 @@
+#include "LinkedLists.h"
+
+LinkedLists::LinkedLists()
+{
+
+}
+
+LinkedLists::~LinkedLists()
+{
+
+}
+
+ListNode* LinkedLists::MakeNode(ListNode** head, int d)
+{
+	ListNode* newNode = new ListNode;
+	ListNode* last = *head;
+
+	newNode->data = d;
+	newNode->next = nullptr;
+
+	if (*head == nullptr)
+	{
+		*head = newNode;
+		return newNode;
+	}
+
+	while (last->next != nullptr)
+	{
+		last = last->next;
+	}
+	last->next = newNode;
+	return newNode;
+}
+
+ListNode* LinkedLists::InsertFirst(ListNode** head, int d)
+{
+	return NULL;
+}
+
+void LinkedLists::InsertAfter(ListNode* lastNode, int d)
+{
+
+}
+
+void LinkedLists::DeleteList(ListNode** node)
+{
+
+}
+
+void LinkedLists::DeleteAfter(ListNode* node)
+{
+
+}
+
+ListNode* LinkedLists::GetNode(ListNode* node, int pos)
+{
+	return NULL;
+}
+
+ListNode* LinkedLists::Find(ListNode* node, int val)
+{
+	return NULL;
+}
+
+void LinkedLists::PrintList(ListNode* node)
+{
+	while (node != nullptr)
+	{
+		std::cout << std::setw(5) << "| " << node->data << " |-->";
+		node = node->next;
+	}
+	if (node == nullptr)
+	{
+		std::cout << "| NULL |" << std::endl;
+	}
+	std::cout << std::endl;
+}
---
+++ LinkedLists/LinkedLists.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "structs.h"
+
+class LinkedLists
+{
+private:
+	ListNode* head = nullptr;
+public:
+	LinkedLists();
+	~LinkedLists();
+
+	ListNode* MakeNode(ListNode** head, int d);
+	ListNode* InsertFirst(ListNode** head, int d);
+	void InsertAfter(ListNode* lastNode, int d);
+	void DeleteList(ListNode** node);
+	void DeleteAfter(ListNode* node);
+	ListNode* GetNode(ListNode* node, int pos);
+	ListNode* Find(ListNode* node, int val);
+	void PrintList(ListNode* node);
+};
