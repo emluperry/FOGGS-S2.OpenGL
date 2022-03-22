043a58212cce91e635b4e4331c50ffdd4a92402b
Emily Perry <emluperry@gmail.com>
2022-03-22 14:24:33 +0000

    BinaryTree Nodes and PreOrder print

---
+++ BinaryTrees/BinaryTree.cpp
@@ -0,0 +1,87 @@
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree()
+{
+
+}
+
+BinaryTree::~BinaryTree()
+{
+	DeleteTree();
+}
+
+void BinaryTree::MakeNode(int d, TreeNode** r)
+{
+	TreeNode* newNode = new TreeNode(d);
+	newNode->leftNode = nullptr;
+	newNode->rightNode = nullptr;
+
+	TreeNode* next = *r;
+	TreeNode* last = *r;
+	bool leftOrRight = false;
+
+	if (*r == nullptr)
+	{
+		*r = newNode;
+		return;
+	}
+
+	while (next != nullptr)
+	{
+		if (next->data > d)
+		{
+			last = next;
+			next = next->leftNode;
+			leftOrRight = false;
+		}
+		else
+		{
+			last = next;
+			next = next->rightNode;
+			leftOrRight = true;
+		}
+	}
+	if (leftOrRight)
+	{
+		last->rightNode = newNode;
+	}
+	else
+	{
+		last->leftNode = newNode;
+	}
+}
+
+void BinaryTree::PrintPreOrder(TreeNode* r)
+{
+	//print r
+	//check left node
+		//print left node
+		//check ITS left node etc
+	//check right node
+		//print right node
+		//check ITS left node etc
+	std::cout << " " << r->data << " ";
+	if (r->leftNode != nullptr)
+	{
+		PrintPreOrder(r->leftNode);
+	}
+	if (r->rightNode != nullptr)
+	{
+		PrintPreOrder(r->rightNode);
+	}
+}
+
+void BinaryTree::PrintInOrder()
+{
+
+}
+
+void BinaryTree::PrintPostOrder()
+{
+
+}
+
+void BinaryTree::DeleteTree()
+{
+
+}
---
+++ BinaryTrees/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "BinaryTree.h"
+#include "structs.h"
+
+int main()
+{
+	TreeNode* root = nullptr;
+	BinaryTree* tree = new BinaryTree();
+	tree->MakeNode(10, &root);
+	tree->MakeNode(6, &root);
+	tree->MakeNode(13, &root);
+	tree->MakeNode(3, &root);
+	tree->MakeNode(8, &root);
+	tree->MakeNode(11, &root);
+	tree->MakeNode(15, &root);
+
+	tree->PrintPreOrder(root);
+}
