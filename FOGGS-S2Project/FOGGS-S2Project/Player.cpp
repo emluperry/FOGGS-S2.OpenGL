4e6e169d0c16852cd6d6ef9a97bde9fe1bb4807c
Osiristic <emluperry@gmail.com>
2022-04-24 16:36:00 +0100

    Basic camera follow and rotation
    Ship moves constantly in a straight line

--- FOGGS-S2Project/FOGGS-S2Project/Player.cpp
+++ FOGGS-S2Project/FOGGS-S2Project/Player.cpp
@@ -2,16 +2,22 @@
 
 Player::Player(Mesh* mesh, Material* material, float x, float y, float z) : SceneObject(mesh, material)
 {
-	_position.x = x;
-	_position.y = y;
-	_position.z = z;
+	_position = { x, y, z };
+
+	_rotation = { 0,0,0 };
+
+	_flightSpeed = 0.1;
+	_turnSpeed = 2;
 }
 
 Player::Player(TexturedMesh* mesh, Texture2D* texture, Material* material, float x, float y, float z) : SceneObject(mesh, texture, material)
 {
-	_position.x = x;
-	_position.y = y;
-	_position.z = z;
+	_position = { x, y, z };
+
+	_rotation = { 0,0,0 };
+
+	_flightSpeed = 0.1;
+	_turnSpeed = 2;
 }
 
 Player::~Player()
@@ -27,6 +33,8 @@ void Player::Draw()
 
 		glPushMatrix();
 		glTranslatef(_position.x, _position.y, _position.z);
+		glRotatef(_rotation.x, 1.0f, 0.0f, 0.0f);
+		glRotatef(_rotation.z, 0.0f, 0.0f, 1.0f);
 		glDrawElements(GL_TRIANGLES, _mesh->indexCount, GL_UNSIGNED_SHORT, _mesh->indices);
 		glPopMatrix();
 
@@ -36,5 +44,25 @@ void Player::Draw()
 
 void Player::Update()
 {
-	_material->shininess += 100;
+	_position.x += _flightSpeed;
+}
+
+void Player::Keyboard(unsigned char key, int x, int y)
+{
+	if (key == 'a')
+	{
+		_rotation.x -= _turnSpeed;
+	}
+	if (key == 'd')
+	{
+		_rotation.x += _turnSpeed;
+	}
+	if (key == 'w')
+	{
+		_rotation.z += _turnSpeed;
+	}
+	if (key == 's')
+	{
+		_rotation.z -= _turnSpeed;
+	}
 }
--- FOGGS-S2Project/FOGGS-S2Project/Player.h
+++ FOGGS-S2Project/FOGGS-S2Project/Player.h
@@ -4,6 +4,10 @@ class Player : public SceneObject
 {
 private:
 	Vector3 _position;
+	Vector3 _rotation;
+
+	float _flightSpeed;
+	float _turnSpeed;
 public:
 	Player(Mesh* mesh, Material* material, float x, float y, float z);
 	Player(TexturedMesh* mesh, Texture2D* texture, Material* material, float x, float y, float z);
@@ -12,5 +16,7 @@ public:
 	void Update() override;
 
 	Vector3 GetPosition() { return _position; }
+
+	void Keyboard(unsigned char key, int x, int y);
 };
 
